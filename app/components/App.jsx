07ead3c56214c359ada120f3ee1e70a06639133c
Andy Tran <transient.tran@gmail.com>
2017-09-06 15:00:26 -0400

    pulled data from LCBO and passing them to Drink components via props

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,22 +5,37 @@ import axios from 'axios'
 class App extends React.Component {
   constructor(props) {
     super();
+
+    this.state = {
+      products: []
+    }
   }
 
-  componentWillMount() {
-
+  componentDidMount() {
+    axios.get("https://lcboapi.com/products?access_key=MDplNzZhNjgwMi05MzMwLTExZTctOTc4NC1iN2U4ZjMxZDA4ODM6WDBwa1hqQTB0N01HOE1VY1JEbmlJd0FHRGR2c0Jhc3NvSmVI")
+      .then(res => {
+        const products = res.data.result
+        this.setState({ products: products })
+        console.log(this.state)
+      })
   }
 
 
   render () {
     return (
       <div className="container">
-        <Drink />
-        <Drink />
-        <Drink />
-        <Drink />
-        <Drink />
-        <Drink />        
+        {this.state.products.map(product =>
+          <Drink 
+          key={product.id} 
+          name={product.name} 
+          thumbnail={product.image_thumb_url} 
+          category={product.tertiary_category} 
+          package={product.package}
+          alcohol={product.alcohol_content}
+          price={product.price_in_cents}
+          is_dead={product.is_dead}
+          />
+        )}     
       </div>
       )
   }
--- app/components/Drink.jsx
+++ app/components/Drink.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class Drink extends React.Component {
+  constructor(props) {
+    super();
+  }
+
   render () {
     return (
       <div className="drink">
-        A Drink
+      <img src={this.props.thumbnail} className="thumbnail" />
+        <h1>{this.props.name}</h1>
+        <p>{this.props.category} <br />
+        {this.props.package} - {this.props.alcohol}</p>
+
+        <p>{this.props.price} <br />
+        {this.props.is_dead}</p>
+
       </div>
       )
   }
